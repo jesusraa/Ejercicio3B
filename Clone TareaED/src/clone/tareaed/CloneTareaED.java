9ada8c9128531cb7294bfbfcf66ea28b4cc7be44
Pablo Alonso <micorreo@iesdoctorbalmis.es>
2019-01-16 11:18:41 +0100

    añado archivo java

---
+++ Clone TareaED/src/clone/tareaed/CloneTareaED.java
@@ -0,0 +1,17 @@
+
+package clone.tareaed;
+
+/**
+ *
+ * @author alumno
+ */
+public class CloneTareaED {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
